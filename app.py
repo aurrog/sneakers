54be58e5f3990765c366d1d485c2373b921127da
aurrog <gnusovme@gmail.com>
2024-04-24 21:28:02 +0300

    update

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
